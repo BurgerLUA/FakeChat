aece4e9d82bbc0060da62fd81fcfafa83c165dfb
BurgerLUA <mattwasnothere@gmail.com>
2017-02-19 13:17:34 -0800

    Fakechat stuff

--- lua/autorun/client/cl_fakechat.lua
+++ lua/autorun/client/cl_fakechat.lua
@@ -10,19 +10,7 @@ net.Receive("FakePlayerChat", function(len)
 	if Victim:Health() <= 0 then
 		DeadText = "*DEAD*"
 	end
-	
-	if LocalPlayer():SteamID() == "STEAM_0:0:15446066" then
-		chat.AddText(team.GetColor(Sender:Team()),Sender:Nick(),Color(255,255,255,255)," made ",team.GetColor(Victim:Team()),Victim:Nick(),Color(255,255,255,255)," say: ")
-	end
-	
-	--[[
-	if Sender ~= LocalPlayer() then
-		Victim = Sender
-	end
-	--]]
-	
 
-	chat.AddText(	Color(255,0,0,255),DeadText,team.GetColor(Victim:Team()),Victim:Nick(),Color(255,255,255,255),":" .. Text
-				)
+	chat.AddText(Color(255,0,0,255),DeadText,team.GetColor(Victim:Team()),Victim:Nick(),Color(255,255,255,255),":" .. Text)
 				
 end)
